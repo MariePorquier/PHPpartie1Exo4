833b219362b25b2ca2b2179a23415daba21567c0
MariePorquier <marieporquier@free.fr>
2019-12-03 09:20:20 +0100

    partie1exo4php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$name = 'Marie';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>PHPpartie1exo4</title>
+  </head>
+  <body>
+      <p>Bonjour <?= $name; ?> Comment vas-tu? </p>
+  </body>
+</html>
